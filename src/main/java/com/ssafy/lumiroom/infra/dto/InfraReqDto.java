2283d869c28b3ed305bc4f8027095cd9a033b672
hye <eunhyekim811@gmail.com>
2026-06-16 02:11:05 +0900

    [Chore] #3: 필요없는 파일 구조 변경

--- src/main/java/com/ssafy/lumiroom/infra/dto/InfraReqDto.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.lumiroom.infra.dto;
-
-public record InfraReqDto (
-        String type,      // cctv, securityLight, streetLight
-        double swLat,     // 남서쪽 위도
-        double swLng,     // 남서쪽 경도
-        double neLat,     // 북동쪽 위도
-        double neLng      // 북동쪽 경도
-) {}
--- src/main/java/com/ssafy/lumiroom/infra/dto/InfraResDto.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.lumiroom.infra.dto;
-
-public record InfraResDto(
-        String id,
-        double latitude,
-        double longitude
-) {}
