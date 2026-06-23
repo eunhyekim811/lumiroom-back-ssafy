4707edc33c9b87d15fd510d7aeb4f76de0665c16
eunhye <eunhyekim811@gmail.com>
2026-06-23 15:57:46 +0900

    [Feat] #12: 매물id로 특정 매물 데이터 조회 api

--- src/main/java/com/ssafy/lumiroom/domain/controller/PropertyController.java
+++ src/main/java/com/ssafy/lumiroom/domain/controller/PropertyController.java
@@ -1,17 +1,22 @@
 package com.ssafy.lumiroom.domain.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.lumiroom.domain.model.dto.PropertyListItemResponse;
 import com.ssafy.lumiroom.domain.model.dto.PropertyListResponse;
 import com.ssafy.lumiroom.domain.model.service.PropertyListService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/properties")
@@ -42,5 +47,11 @@ public class PropertyController {
             @Parameter(description = "페이지 크기, 최대 500", example = "100")
             @RequestParam(defaultValue = "100") int size) {
         return propertyListService.findProperties(lat, lng, radius, page, size);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<PropertyListItemResponse> getPropertyById(@PathVariable Long id) {
+    	PropertyListItemResponse property = propertyListService.getPropertyDetail(id);
+        return property != null ? ResponseEntity.ok(property) : ResponseEntity.notFound().build();
     }
 }
